917927b7c1cdb0f3ed138cb6e5b3972a3ff07489
Flávio Juvenal <flaviojuvenal@gmail.com>
2016-06-10 17:36:00 -0300

    Use https on submodules and remove unused files

--- serve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-trap 'kill $(jobs -p)' EXIT  # kill all background process on exit
-
-python -m http.server &
-when-changed -s Python\ WAT.ipynb jupyter nbconvert Python\ WAT.ipynb --to slides &
-echo 'Open on your browser: http://localhost:8000/Python WAT.slides.html'
-wait
